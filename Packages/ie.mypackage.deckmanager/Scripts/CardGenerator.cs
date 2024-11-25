540e24ed757978b15c0c0d53fd728796baf1392d
DarraghMcKernan <dmckernan77@gmail.com>
2024-11-25 10:46:42 +0000

    template

--- Packages/ie.mypackage.deckmanager/Scripts/CardGenerator.cs
+++ Packages/ie.mypackage.deckmanager/Scripts/CardGenerator.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class CardGenerator : MonoBehaviour
 {
+    public struct Card
+    {
+        public string Type;//face card subject type etc
+        public int Value;//card num or just a number to easily find
+        
+        public string Question;//the question associated with this card
+        public string Answer;//the correct answer
+
+        public string FalseAnswer;//false answer
+        public string FalseAnswer1;//extra false answers if needed
+        public string FalseAnswer2;
+        public string FalseAnswer3;
+        public string FalseAnswer4;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +28,10 @@ public class CardGenerator : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void addNewCards(string t_type)
+    {
+
     }
 }
