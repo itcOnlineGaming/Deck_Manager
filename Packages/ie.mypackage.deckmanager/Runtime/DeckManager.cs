0a8c12103c0d3581fe4769dd19be2f46dab5c857
DarraghMcKernan <dmckernan77@gmail.com>
2024-12-07 21:34:35 +0000

    odd amounts of values

--- Packages/ie.mypackage.deckmanager/Runtime/DeckManager.cs
+++ Packages/ie.mypackage.deckmanager/Runtime/DeckManager.cs
@@ -23,40 +23,8 @@ public class DeckManager : MonoBehaviour
         public string FalseAnswer2;//extra false answers if needed
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void addNewCards(string t_type, int t_value,string t_question,string t_answer,string t_falseAnswer, string t_falseAnswer2)
     {
-        //if(allTypes.Count == 0)
-        //{
-        //    allTypes.Add(t_type);
-        //}
-        //else
-        //{
-        //    bool alreadyInUse = false;
-        //    for(int index = 0; index < allTypes.Count; index++)
-        //    {
-        //        if (t_type == allTypes[index] && alreadyInUse == false)
-        //        {
-        //            alreadyInUse = true;
-        //        }
-        //    }
-        //    if(alreadyInUse == false)//new type found
-        //    {
-        //        allTypes.Add(t_type);
-        //    }
-        //}
-
         Card tempCard = new Card();
         tempCard.Type = t_type;
         tempCard.Value = t_value;
@@ -68,19 +36,12 @@ public class DeckManager : MonoBehaviour
         cardList.Add(tempCard);
     }
 
-    public string generateBulkCards(List<string> t_type, List<int> t_value, List<string> t_question, List<string> t_answer, List<string> t_falseAnswer, List<string> t_falseAnswer2)
+    public void generateBulkCards(List<string> t_type, List<int> t_value, List<string> t_question, List<string> t_answer, List<string> t_falseAnswer, List<string> t_falseAnswer2)
     {
-        if(t_type.Count != t_value.Count)//make sure all the lists are the same size before proceeding
-        {
-            return "One or more of the provided lists are not the same size";
-        }
-
         for(int index = 0;index < t_type.Count;index++)
         {
             addNewCards(t_type[index], t_value[index], t_question[index], t_answer[index],t_falseAnswer[index], t_falseAnswer2[index]);
         }
-
-        return "Lists all Valid";//all lists are valid
     }
 
     public Card getSpecifiedCard(string t_type,int t_number)
